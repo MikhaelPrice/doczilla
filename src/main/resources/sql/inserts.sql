37d7bb68758df647c1bbb9e2674a46e157a1847d
MichaelPrice <mikhail.a.prus@gmail.com>
2023-12-28 20:27:58 +0300

    #Task 2 [+] Added DB with table students

---
+++ src/main/resources/sql/inserts.sql
@@ -0,0 +1,5 @@
+insert into students values (1, 'Michail', 'Prus', 'Andreevich', '24.07.2002', 052)
+insert into students values (2, 'Nighel', 'Dickens', 'Antonovich', '02.01.2003', 012)
+insert into students values (3, 'Meredit', 'Grey', 'Nikitischna', '15.11.1989', 014)
+insert into students values (4, 'Derek', 'Shepard', 'Vasilevich', '11.05.1982', 015)
+insert into students values (5, 'Mark', 'Sloan', 'Dmitrievich', '08.09.1979', 015)
---
+++ src/main/resources/sql/students.sql
@@ -0,0 +1,8 @@
+create table Students(
+    id int unique not null,
+    name varchar not null,
+    surname varchar not null ,
+    lastname varchar not null,
+    birthday date,
+    group_number int
+)
